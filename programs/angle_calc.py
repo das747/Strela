bd4b51cfff488ef780c71e4bfc05e79c97ed2b78
das747 <40293014+das747@users.noreply.github.com>
2018-12-01 00:00:02 +0700

    driver

--- programs/angle_calc.py
+++ programs/angle_calc.py
@@ -1,5 +1,6 @@
 import math
 angle=int(input())
-M=[45,135,225,315]
+M = [325, 35, 35, 325]
+sp_ref = 200
 for i in M:
-    print(math.cos(math.pi*(angle-i)/180))
+    print(math.cos(math.pi * (angle-i)/180) * sp_ref)
